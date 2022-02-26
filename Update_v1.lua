31e39803769eef06f1a266f52116ecc318d18981
TheProfessionalDumbass <93713321+TheProfessionalDumbass@users.noreply.github.com>
2022-02-25 19:55:12 -0800

    Update Update_v1.lua

--- Update_v1.lua
+++ Update_v1.lua
@@ -1,3 +1,3 @@
 
-
-venyx:Notify("Cheese Hub Free", "Version: 1.0.0a")  
+print('xrpl is here ??')
+venyx:Notify("Cheese Hub Free", "Version: 1.0.0a | Nice! Your using the latest version. :) Enjoy!")  
